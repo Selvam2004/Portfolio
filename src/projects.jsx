0d84a60b93b14ec592bb0f2bc78e42001be16feb
Selvam2004 <selvam032004@gmail.com>
2024-03-18 00:26:29 +0530

    first commit

--- src/projects.jsx
+++ src/projects.jsx
@@ -17,7 +17,7 @@ export default function Projects(){
                             <div className="pro-card"> 
                             <h3 style={{color:"aquamarine"}} >Trip Advisor</h3>
                             <p>This project offers detailed destination information alongside hotel and restaurant booking functionality,empowering users to plan their trips conveniently in one platform</p>
-                            <button className="pro-btn">Go to Project</button>
+                            <a href="http://trip-advisor-phi.vercel.app"  target='_blank'  rel="noreferrer" ><button className="pro-btn">Go to Project</button></a>
                             </div>
                         </Col>
                         <Col  md={5}><img src={require('./images/pro1.png')} className="pro-ig" alt="project"/>
@@ -33,7 +33,7 @@ export default function Projects(){
                             <div className="pro-card"> 
                             <h3 style={{color:"aquamarine"}} >Personal Library</h3>
                             <p>The Personal Library help users organize and manage their personal book collections. Users can add books to their library, mark their read status, search for specific books, and update their collection as needed.</p>
-                            <button className="pro-btn">Go to Project</button>
+                            <a href="https://github.com/Selvam2004/PersonalLibrary"  target='_blank'  rel="noreferrer" ><button className="pro-btn">Go to Project</button></a>
                             </div>
                         </Col>
                         <Col  md={5}><img src={require('./images/pro2.png')} className="pro-ig" alt="project"/>
@@ -49,7 +49,7 @@ export default function Projects(){
                             <div className="pro-card"> 
                             <h3 style={{color:"aquamarine"}} >Quizz</h3>
                             <p>In this Quizz app each question comes with a time limit, adding excitement and challenge to your quiz experience.At the end of each quiz, your score is displayed, allowing you to see how well you did..</p>
-                            <button className="pro-btn">Go to Project</button>
+                            <a href="https://github.com/Selvam2004/JSQuiz"  target='_blank'  rel="noreferrer" ><button className="pro-btn">Go to Project</button></a>
                             </div>
                         </Col>
                         <Col  md={5}><img src={require('./images/pro3.png')} className="pro-ig" alt="project"/>
@@ -65,7 +65,7 @@ export default function Projects(){
                             <div className="pro-card"> 
                             <h3 style={{color:"aquamarine"}} >Blog Website</h3>
                             <p>The Blog Website offers a dynamic space where both creators and readers converge to exchange ideas, inspire one another, and expand their horizons.This empowers you to publish your own blogs effortlessly. </p>
-                            <button className="pro-btn">Go to Project</button>
+                            <a href="https://github.com/Selvam2004/BlogSite" target='_blank'  rel="noreferrer" ><button className="pro-btn">Go to Project</button></a>
                             </div>
                         </Col>
                         <Col  md={5}><img src={require('./images/pro4.png')} className="pro-ig" alt="project"/>
